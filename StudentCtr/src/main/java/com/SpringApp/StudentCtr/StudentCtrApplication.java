ab8cdc409f8cfc242e81835a4d7e1765554bdc30
Radeonxz <xuanxxzhao@gmail.com>
2022-08-29 19:32:48 -0400

    init: StudentCtr

---
+++ StudentCtr/src/main/java/com/SpringApp/StudentCtr/StudentCtrApplication.java
@@ -0,0 +1,13 @@
+package com.SpringApp.StudentCtr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCtrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCtrApplication.class, args);
+	}
+
+}
