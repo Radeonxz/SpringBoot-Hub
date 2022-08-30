4c885227b0bab5cb911016bf221a9b5eb0184dcd
Radeonxz <xuanxxzhao@gmail.com>
2022-08-29 22:35:27 -0400

    chore: add service and repository

--- StudentCtr/src/main/java/com/SpringApp/StudentCtr/StudentCtrApplication.java
+++ StudentCtr/src/main/java/com/SpringApp/StudentCtr/StudentCtrApplication.java
@@ -1,13 +1,33 @@
 package com.SpringApp.StudentCtr;
 
+//import com.SpringApp.StudentCtr.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.time.LocalDate;
+//import java.time.Month;
+//import java.util.List;
 
 @SpringBootApplication
+//@RestController
 public class StudentCtrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentCtrApplication.class, args);
 	}
+
+//	@GetMapping
+//	public List<String> indexRoute() {
+//		return List.of("Index", "Route");
+//	}
+
+//	@GetMapping
+//	public List<Student> indexRoute() {
+//		return List.of(
+//				new Student(1L, "foo", "foo@foo.com", LocalDate.of(1000, Month.JANUARY, 1), 10)
+//		);
+//	}
 
 }
---
+++ StudentCtr/src/main/java/com/SpringApp/StudentCtr/student/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.SpringApp.StudentCtr.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
